474b69c1c603729ae3a48fab2fee7025bf04813d
Crowdbotics <team@crowdbotics.com>
2020-11-25 02:07:13 +0000

    Update models

--- backend/vendisses_22962/wsgi.py
+++ backend/vendisses_22962/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vendisses_22962.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vendisses_22962.settings")
 
 application = get_wsgi_application()
